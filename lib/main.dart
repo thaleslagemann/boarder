cf347408c830851b8c410760ee0b0033bb15aa7e
Thales Lagemann <thaleslagemann@gmail.com>
2023-08-29 17:17:03 -0300

    add buttons, list and rename tabs.

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
+      create: (context) => AppState(),
       child: MaterialApp(
         title: 'Kanban Flutter',
         theme: ThemeData(
@@ -25,22 +25,157 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyAppState extends ChangeNotifier {
+class AppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  var boardsList = <String>[];
+  var settingsList = <String>[];
+
+  addBoard() {
+    boardsList.add(WordPair.random().toString());
+    notifyListeners();
+  }
+
+  doSomething() {
+    print('Button was clicked.');
+  }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = BoardsPage();
+        break;
+      case 1:
+        page = SettingsPage();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                extended: constraints.maxWidth >= 600,
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.description),
+                    label: Text('Boards'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.settings_rounded),
+                    label: Text('Settings'),
+                  ),
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) {
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+}
+
+class BoardsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<AppState>();
+
+    if (appState.boardsList.isEmpty) {
+      return Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+        body: Center(
+          child: Text('No boards yet.'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(50.0)),
+          ),
+          backgroundColor: Theme.of(context).colorScheme.surface,
+          onPressed: () {
+            appState.addBoard();
+          },
+          child: Icon(Icons.add),
+        ),
+      );
+    }
 
     return Scaffold(
-      body: Column(
+      backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+      body: ListView(
+        shrinkWrap: true,
         children: [
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
+          for (var board in appState.boardsList)
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: TextButton(
+                onPressed: appState.doSomething(),
+                child: ListTile(
+                  leading: Icon(Icons.description),
+                  title: Text(board.toString()),
+                ),
+              ),
+            ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(50.0)),
+        ),
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        onPressed: () {
+          appState.addBoard();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class SettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<AppState>();
+
+    if (appState.settingsList.isEmpty) {
+      return Center(
+        child: Text('No settings yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        for (var settings in appState.settingsList)
+          ListTile(
+            leading: Icon(Icons.settings_rounded),
+            title: Text(settings),
+          ),
+      ],
     );
   }
 }
