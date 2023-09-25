b386e36fba8e000fc5c76a9063993123559fe01f
Thales Lagemann <thaleslagemann@gmail.com>
2023-09-25 16:27:13 -0300

    rework: add new testboards system, remove former system

--- lib/test_board_view.dart
+++ lib/test_board_view.dart
@@ -1,6 +1,15 @@
 import 'package:drag_and_drop_lists/drag_and_drop_lists.dart';
 import 'package:flutter/material.dart';
 
+class HeaderStructure {
+  final int id;
+  final String name;
+  late DragAndDropList content;
+
+  HeaderStructure(
+      {required this.id, required this.name, required this.content});
+}
+
 class BasicExample extends StatefulWidget {
   const BasicExample({Key? key}) : super(key: key);
 
@@ -10,14 +19,128 @@ class BasicExample extends StatefulWidget {
 
 class _BasicExample extends State<BasicExample> {
   List<DragAndDropList> _contents = [];
+  List<HeaderStructure> headers = [];
+  static Size screenSize = WidgetsBinding.instance.window.physicalSize;
+  double width = screenSize.width;
+  double height = screenSize.height;
+  final TextEditingController _textFieldController = TextEditingController();
+  String newHeaderName = '';
 
   @override
   void initState() {
     super.initState();
   }
 
-  pushIntoList() {
-    _contents.add(DragAndDropList(
+  bool containsElement(List<HeaderStructure> list, elementToCheck) {
+    for (var element in list) {
+      if (element.id == elementToCheck) {
+        return true;
+      }
+    }
+    return false; // Element not found in any pair
+  }
+
+  int getSequentialID(List<HeaderStructure> list, int id) {
+    if (containsElement(list, id)) {
+      id = id + 1;
+      return getSequentialID(list, id);
+    }
+    return id;
+  }
+
+  Future<void> _displayTextInputDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('New header name'),
+            content: TextField(
+              onChanged: (value) {
+                setState(() {
+                  newHeaderName = value;
+                });
+              },
+              controller: _textFieldController,
+              decoration: const InputDecoration(hintText: "Header name"),
+            ),
+            actions: <Widget>[
+              TextButton(
+                style: TextButton.styleFrom(foregroundColor: Colors.red),
+                child: const Text('cancel'),
+                onPressed: () {
+                  setState(() {
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+              TextButton(
+                child: const Text('ok'),
+                onPressed: () {
+                  setState(() {
+                    newHeaderName = _textFieldController.text;
+                    print(newHeaderName);
+                    pushHeaderIntoList(newHeaderName);
+                    newHeaderName = '';
+                    _textFieldController.clear();
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  pushHeaderIntoList(String headerName) {
+    DragAndDropList content = generateHeader(headerName);
+    headers.add(HeaderStructure(
+        id: getSequentialID(headers, 0), name: headerName, content: content));
+    for (var i = 0; i < headers.length; i++) {
+      print('[${headers[i].id}, ${headers[i].name}]');
+    }
+  }
+
+  pushItemIntoHeader(int headerIndex) {
+    headers[headerIndex].content.children.add(
+          DragAndDropItem(
+            child: Row(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15.0, top: 10.0),
+                      child: Text('item'),
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: WidgetsBinding.instance.window.physicalSize.width *
+                          0.27,
+                    )
+                  ],
+                ),
+                Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      IconButton(
+                          onPressed: () => setState(() {}),
+                          icon: Icon(Icons.keyboard_arrow_down_sharp)),
+                    ])
+              ],
+            ),
+          ),
+        );
+  }
+
+  generateHeader(String headerName) {
+    return DragAndDropList(
       header: Row(
         children: <Widget>[
           const Expanded(
@@ -26,7 +149,7 @@ class _BasicExample extends State<BasicExample> {
           ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-            child: Text('Header'),
+            child: Text(headerName),
           ),
           const Expanded(
             flex: 1,
@@ -34,18 +157,20 @@ class _BasicExample extends State<BasicExample> {
           ),
         ],
       ),
-      children: <DragAndDropItem>[
-        DragAndDropItem(
-          child: Text('item'),
-        ),
-      ],
-    ));
+      children: <DragAndDropItem>[],
+    );
+  }
+
+  List<DragAndDropList> listHeaders() {
+    List<DragAndDropList> list = [];
+    for (var element in headers) {
+      list.add(element.content);
+    }
+    return list;
   }
 
   @override
   Widget build(BuildContext context) {
-    var deviceWidth = MediaQuery.of(context).size.width;
-    pushIntoList();
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         shape: RoundedRectangleBorder(
@@ -54,32 +179,33 @@ class _BasicExample extends State<BasicExample> {
         foregroundColor: Theme.of(context).colorScheme.surface,
         backgroundColor: Color(0xFF4FC3F7),
         onPressed: () {
-          pushIntoList();
+          _displayTextInputDialog(context);
+          print(newHeaderName);
         },
         child: Icon(Icons.add),
       ),
       body: SafeArea(
-        child: DragAndDropLists(
-          children: _contents,
-          onItemReorder: _onItemReorder,
-          onListReorder: _onListReorder,
-        ),
-      ),
+          child: DragAndDropLists(
+        children: listHeaders(),
+        onItemReorder: _onItemReorder,
+        onListReorder: _onListReorder,
+      )),
     );
   }
 
   _onItemReorder(
       int oldItemIndex, int oldListIndex, int newItemIndex, int newListIndex) {
     setState(() {
-      var movedItem = _contents[oldListIndex].children.removeAt(oldItemIndex);
-      _contents[newListIndex].children.insert(newItemIndex, movedItem);
+      var movedItem =
+          headers[oldListIndex].content.children.removeAt(oldItemIndex);
+      headers[newListIndex].content.children.insert(newItemIndex, movedItem);
     });
   }
 
   _onListReorder(int oldListIndex, int newListIndex) {
     setState(() {
-      var movedList = _contents.removeAt(oldListIndex);
-      _contents.insert(newListIndex, movedList);
+      var movedList = headers.removeAt(oldListIndex);
+      headers.insert(newListIndex, movedList);
     });
   }
 }
